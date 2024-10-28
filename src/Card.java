8831d286862d33238cc1429f8da82017ce53c3e6
paulawang <paulatwang@gmail.com>
2024-10-27 21:11:24 -0400

    Added getter methods to Card.java and set points values in constructor

--- src/Card.java
+++ src/Card.java
@@ -6,8 +6,24 @@ public class Card {
     public Card(String suit, String value) {
         this.suit = suit;
         this.value = value;
+
+        // calculate points
+        if (this.value.equals("A")){
+            this.points = 11;
+        } else if (this.value.equals("J") || this.value.equals("Q") || this.value.equals("K")){
+            this.points = 10;
+        } else {
+            this.points = Integer.parseInt(this.value);
+        }
     }
 
+    public String getCard(){
+        return this.suit + this.value;
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
 
 
 }
