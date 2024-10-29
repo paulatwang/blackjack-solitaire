2b2df9d9d439ad480aac3a1103dbbae59b8ba080
paulawang <paulatwang@gmail.com>
2024-10-28 22:17:09 -0400

    Added placeholder constructor

--- src/Card.java
+++ src/Card.java
@@ -1,28 +1,42 @@
 public class Card {
-    private String suit;
+    private String label;
     private String value;
     private int points;
 
-    public Card(String suit, String value) {
-        this.suit = suit;
-        this.value = value;
 
-        // calculate points
-        if (this.value.equals("A")){
+    public Card(String placeholder){
+        this.label = placeholder;
+        this.value = placeholder;
+        this.points = 0;
+    }
+
+    public Card(String suit, String value) {
+        this.label = value + suit;
+        this.value = value;
+        /*
+        * Calculate points
+        * - set initial points of Ace to 11
+        * - when playing, if row/column sum has A and exceeds 21, -10 from sum
+        * */
+        if (value.equals("A")){
             this.points = 11;
-        } else if (this.value.equals("J") || this.value.equals("Q") || this.value.equals("K")){
+        } else if (value.equals("J") || value.equals("Q") || value.equals("K")){
             this.points = 10;
         } else {
-            this.points = Integer.parseInt(this.value);
+            this.points = Integer.parseInt(value);
         }
     }
 
-    public String getCard(){
-        return this.suit + this.value;
+    public String getLabel(){
+        return this.label;
     }
 
     public int getPoints(){
         return this.points;
+    }
+
+    public String getValue(){
+        return this.value;
     }
 
 
